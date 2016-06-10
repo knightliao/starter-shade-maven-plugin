82240855144764530df98f5406c1081db3df12d3
knightliao <knightliao@gmail.com>
2016-06-10 11:31:42 +0800

    re

--- src/main/resources/META-INF/start.sh
+++ src/main/resources/META-INF/start.sh
@@ -22,5 +22,7 @@ fi
 #export JAVA_OPTS="$JAVA_OPTS -XX:+UseParallelGC -XX:+UseParallelOldGC -XX:ParallelGCThreads=2"
 #export JAVA_OPTS="$JAVA_OPTS -XX:+PrintGCDetails -XX:+PrintGCTimeStamps"
 
-echo "nohup java $JAVA_OPTS -jar ${CUR_BUNDLE_NAME}  >> log_`date +%s`.log 2>&1 &"
-nohup java $JAVA_OPTS -jar ${CUR_BUNDLE_NAME} >> log_`date +%s`.log 2>&1 &
+log_name=`date "+%Y%m%d%H%M%S"`
+
+echo "nohup java $JAVA_OPTS -jar ${CUR_BUNDLE_NAME}  >> log_"${log_name}".log 2>&1 &"
+nohup java $JAVA_OPTS -jar ${CUR_BUNDLE_NAME} >> log_"${log_name}".log 2>&1 &
